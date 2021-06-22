635e83cf98183835495e1482af3e17bb45de678c
Monty Lennie <montylennie@gmail.com>
2021-06-22 21:50:14 +0000

    update smtp settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
      :address              => ENV['SMTP_ADDRESS'],
-     :domain               => ENV['SMTP_DOMAIN'],
+     :domain               => ENV['APP_DOMAIN'],
      :port                 => ENV['SMTP_PORT'],
      :user_name            => ENV['SMTP_USERNAME'],
      :password             => ENV['SMTP_PASSWORD'],
