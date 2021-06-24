9d91ddf2cc773220150d0c33e87ab09464c8dacd
Monty Lennie <montylennie@gmail.com>
2021-06-24 22:48:42 +0000

    finish first draft password reset endpoints and resend email confirm email

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -43,6 +43,35 @@ class Api::V1::UsersController < Api::V1::ApiApplicationController
 
   end
 
+  def resend_confirm_email
+    user = User.find_by(email: params[:email])
+
+    error = false
+    if !params[:email]
+      error = "email required"
+    elsif !user
+      error = "no user with that email"
+    elsif user.confirmed_at
+      error = "user already confirmed"
+    end
+
+    if error
+      json = { error: true, message: error }.to_json
+      render json: json, status: 400
+    else
+      if user.save
+        user.confirmation_token = User.create_new_token("confirmation_token")
+        user.confirmation_sent_at = Time.now
+        UserMailer.with(user: user).confirmation_email.deliver_later
+        render json: {}, status: 204
+      else
+        json = { error: true, message: user.errors.full_messages }.to_json
+        render json: json, status: :bad_request
+      end
+    end
+
+  end
+
   def confirm_email
     token = params[:token]
     user = User.find_by(confirmation_token: token)
@@ -64,6 +93,66 @@ class Api::V1::UsersController < Api::V1::ApiApplicationController
       user.confirmed_at = Time.now
       user.confirmation_token = nil
       user.confirmation_sent_at = nil
+      if user.save
+        render json: {}, status: 204
+      else
+        json = { error: true, message: user.errors.full_messages }.to_json
+        render json: json, status: 500
+      end
+    end
+  end
+
+  def initiate_password_reset
+    token = params[:token]
+    user = User.find_by(reset_password_confirm_email_token: token)
+    expired = Time.now.to_i > (user.reset_password_confirm_email_token_sent_at
+                               + 2.hours).to_i
+
+    error = false
+    if !token || !user
+      error = "token invalid"
+    elsif expired
+      error = "token expired"
+    end
+
+    #TODO: look up how to redirect to url
+    if error
+      redirect_to: ENV["FRONTEND_WEB_URL"] + "?error=#{error}"
+    else
+      user.reset_password_confirm_email_token = nil
+      user.reset_password_confirm_email_token_sent_at = nil
+      user.reset_password_token = User.create_new_token("reset_password_token")
+      user.reset_password_sent_at = Time.now
+      if user.save
+        redirect_to: ENV["FRONTEND_WEB_URL"] + "?token=#{user.reset_password_token}"
+      else
+        redirect_to: ENV["FRONTEND_WEB_URL"] + "?error=could not update user"
+      end
+    end
+  end
+
+  def update_password
+    token = params[:token]
+    user = User.find_by(reset_password_token: token)
+    expired = Time.now.to_i > (user.reset_password_sent_at + 2.hours).to_i
+
+    error = false
+    if !token || !user
+      error = "token invalid"
+    elsif expired
+      error = "token expired"
+    elsif !params[:password] || params[:password].count < 6
+      error = "password needs to be at least 6 characters"
+    end
+
+    if error
+      json = { error: true, message: error }.to_json
+      render json: json, status: 400
+    else
+      user.reset_password_token = nil
+      user.reset_password_sent_at = nil
+      # TODO: look up how to update password
+      user.password = params[:password]
       if user.save
         render json: {}, status: 204
       else
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create] do
           collection do
+            # CONFIRM EMAIL ADDRESS
             get 'confirm_email/:token', to: "users#confirm_email",
                  as: :confirm_email
+            post 'resend_confirm_email/:token', to: "users#resend_confirm_email",
+                 as: :resend_confirm_email
+
             # PASSWORD RESET
             # User has entered email. We send email to confirm
             post 'send_password_reset_email',
@@ -18,8 +22,8 @@ Rails.application.routes.draw do
             get 'initiate_password_reset', to: "users#initiate_password_reset",
                  as: :initiate_password_reset
             # User has submitted a new password
-            post '', to: "users#initiate_password_reset",
-                 as: :initiate_password_reset
+            post '', to: "users#update_password",
+                 as: :update_password
           end
       end
     end
