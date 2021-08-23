ac15734b141a38d994a1d0760ab71cc1906bc269
Monty Lennie <montylennie@gmail.com>
2021-08-23 23:14:43 +0000

    remove university as required for a review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  belongs_to :university
+  belongs_to :university, optional: true
   belongs_to :writer
   belongs_to :photographer
   has_many :images
 
-  validates_presence_of :restaurant_id, :university_id, :writer_id,
-                        :photographer_id
+  validates_presence_of :restaurant_id, :writer_id, :photographer_id
 
   accepts_nested_attributes_for :images, :allow_destroy => true
 
