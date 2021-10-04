564f0720f0cefe6770015afc3b0dc5e53386cf0e
Monty Lennie <montylennie@gmail.com>
2021-10-04 13:13:19 +0000

    write next steps notes

--- app/controllers/api/v1/review_happened_confirmations_controller.rb
+++ app/controllers/api/v1/review_happened_confirmations_controller.rb
@@ -2,6 +2,17 @@ class Api::V1::ReviewHappenedConfirmationsController < Api::V1::ApiApplicationCo
 
   def respond
     binding.pry
+    token = params[:token]
+    response = params[:response]
+    #validate token and response
+    #
+    # if reviewed
+    # set status to reviewed
+    # send out restaurant and creators post reviewed emails
+    #
+    # if wasn't reviewed
+    # need to update status to not reviewed needs rescheduling
+    # send email to admins
   end
 
 end
