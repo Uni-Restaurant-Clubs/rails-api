a483d9ec03eace3ef1408646285945cb4643890b
Monty Lennie <montylennie@gmail.com>
2021-10-26 17:20:38 +0000

    install bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap';
 
 Rails.start()
 Turbolinks.start()
