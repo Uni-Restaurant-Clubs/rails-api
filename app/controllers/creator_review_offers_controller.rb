cf027e240e86533de563ad372815d25596608917
Monty Lennie <montylennie@gmail.com>
2021-11-05 18:23:43 +0000

    start adding validation code for offer form

--- app/controllers/creator_review_offers_controller.rb
+++ app/controllers/creator_review_offers_controller.rb
@@ -1,10 +1,44 @@
 class CreatorReviewOffersController < ApplicationController
 
   def edit
-    @offer = CreatorReviewOffer.find_by(token: params[:id])
+    @token = params[:id]
+    @offer = CreatorReviewOffer.find_by(token: @token)
+    if !@offer
+      @error = "Offer could not be found"
+    elsif @offer.responded_at
+      # verify it has not been responded to already
+      @error = "This offer has already been responded to"
+    elsif Time.now > @offer.created_at + 2.days
+      # validate date (can't be more than 2 days old)
+      @error = "This offer has expired since it was sent more than 2 days ago."
+    end
   end
 
   def update
     binding.pry
+    # verify it has not been responded to already
+    # update offer
+    # update responded at
+    # check for match
+    #
+    # if match, update final scheduled time
+    # send confirmation emails to restaurant and creators
+    # create and send google events
+    #
+    # if no match
+    # send out to everyone
+    # first response gets it
   end
+
+  # strong parameters for review offer
+  private
+
+    def review_offer_params
+      params.require(:creator_review_offer)
+        .permit(:option_one_response, :option_two_response,
+                :option_three_response, :not_available_for_any_options,
+                :does_not_want_to_review_this_restaurant,
+                :does_not_want_to_review_reason, :notes)
+            .to_h.deep_transform_keys!(&:underscore)
+    end
 end
