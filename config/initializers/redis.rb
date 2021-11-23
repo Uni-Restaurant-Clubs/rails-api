d4886a8ffb62eaa8844014fd6188aa22a20f9dcd
Monty Lennie <montylennie@gmail.com>
2021-11-23 16:55:07 +0000

    add space so can deploy

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,3 @@
-$redis = Redis.new(url: ENV["REDIS_PROVIDER"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
+$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+
