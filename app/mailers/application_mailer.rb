5ade5350a52518a9d26224be808b0b3c44ee7cc8
Monty Lennie <montylennie@gmail.com>
2022-01-21 18:30:27 +0000

    add message id to mailer header

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
+require 'digest/sha2'
+
 class ApplicationMailer < ActionMailer::Base
   default from: 'Uni Restaurant Club <hello@unirestaurantclub.com>'
+  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@unirestaurantclub.com"
   layout 'mailer'
 end
