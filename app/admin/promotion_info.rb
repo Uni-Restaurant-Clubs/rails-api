1bf5cd1be678ddf9818e236fed2635a25edb3ce8
Monty Lennie <montylennie@gmail.com>
2022-02-13 19:21:09 +0000

    update fields for promotion info admin page

--- app/admin/promotion_info.rb
+++ app/admin/promotion_info.rb
@@ -14,38 +14,28 @@ ActiveAdmin.register PromotionInfo do
     column :promotion_intro_email_sent_at
     column :restaurant_responded_at
     column :notes
-    column :roles do |role|
-      role.admin_users
-    end
     actions
   end
 
   show do
     attributes_table do
-      row :name
-      row :description
-      table_for role.admin_user_roles do
-        column "Admin Users" do |admin_user_role|
-          admin_user_role.admin_user
-        end
+      row :restaurant do |promotion_info|
+        link_to promotion_info.restaurant, admin_restaurant_path(promotion_info.restaurant.id)
       end
+
+      row :restaurant_status
+      row :promotion_intro_email_sent_at
+      row :restaurant_responded_at
+      row :notes
     end
   end
 
   form do |f|
     f.semantic_errors *f.object.errors.keys
-    f.inputs 'Details' do
-      f.input :name
-      f.input :description
-    end
-    f.inputs 'Roles' do
-      f.has_many :admin_user_roles, heading: false,
-                              remove_record: true do |a|
-        a.input :role_id, :input_html => { :value => f.object.id }, as: :hidden
-        a.input :admin_user
-
-      end
-    end
+    f.input :restaurant_status
+    f.input :promotion_intro_email_sent_at
+    f.input :restaurant_responded_at
+    f.input :notes
     f.actions
   end
 end
