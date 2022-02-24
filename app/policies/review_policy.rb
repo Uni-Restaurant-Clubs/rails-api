c7354bfa563490962f3e1bf69541688781988f3f
Monty Lennie <montylennie@gmail.com>
2022-02-24 17:59:31 +0000

    allow review admin to update reviews

--- app/policies/review_policy.rb
+++ app/policies/review_policy.rb
@@ -10,36 +10,42 @@ class ReviewPolicy < ApplicationPolicy
   def index?
     return true if @user.super_admin?
     return true if @user.creators_admin?
+    return true if @user.restaurant_reviews_admin?
     false
   end
 
   def show?
     return true if @user.super_admin?
     return true if @user.creators_admin?
+    return true if @user.restaurant_reviews_admin?
     false
   end
 
   def new?
     return true if @user.super_admin?
     return true if @user.creators_admin?
+    return true if @user.restaurant_reviews_admin?
     false
   end
 
   def create?
     return true if @user.super_admin?
     return true if @user.creators_admin?
+    return true if @user.restaurant_reviews_admin?
     false
   end
 
   def edit?
     return true if @user.super_admin?
     return true if @user.creators_admin?
+    return true if @user.restaurant_reviews_admin?
     false
   end
 
   def update?
     return true if @user.super_admin?
     return true if @user.creators_admin?
+    return true if @user.restaurant_reviews_admin?
     false
   end
 
@@ -51,6 +57,7 @@ class ReviewPolicy < ApplicationPolicy
   def update_review?
     return true if @user.super_admin?
     return true if @user.creators_admin?
+    return true if @user.restaurant_reviews_admin?
     false
   end
 
